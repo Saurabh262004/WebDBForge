405f730fc1e73657dd065a81c08086d544a7e8c6
Saurabh262004 <saurabhrjadhav26@gmail.com>
2025-12-18 13:11:06 +0530

    -

--- tests/test.py
+++ tests/test.py
@@ -4,6 +4,18 @@ from Implementation.DBStructure import make
 with open('testDSLs/structure.json', 'rb') as structureFile:
   structure = orjson.loads(structureFile.read(-1))
 
-  DB = make(structure)
+  references = {
+    "a": [
+      1, 2, 3
+    ],
+    "b": [
+      '4', '5', '6'
+    ],
+    "c": [
+      6, 7, 8
+    ]
+  }
+
+  DB = make(structure, references)
 
   print(DB)
