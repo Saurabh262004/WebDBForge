3ea46c4424069a1d20e6097059efc02a5e224472
Saurabh262004 <saurabhrjadhav26@gmail.com>
2026-01-06 13:50:57 +0530

    update imports

--- WebDBForge/__init__.py
+++ WebDBForge/__init__.py
@@ -1,3 +1,4 @@
 from WebDBForge.Sanitizer import Sanitizer
 from WebDBForge.Scrapers import Fetcher, ImageCollector
+from WebDBForge.Nav import SoupNavigator
 from WebDBForge.Node import NodeEvaluator
