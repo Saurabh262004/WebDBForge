e7c93eff0a5d5003fa9ec43dbd9fc8db72ff8534
Saurabh262004 <saurabhrjadhav26@gmail.com>
2026-04-16 16:05:15 +0530

    change headers

--- WebDBForge/Scrapers/Fetcher.py
+++ WebDBForge/Scrapers/Fetcher.py
@@ -3,10 +3,13 @@ import cloudscraper
 
 # standard headers for all requests to mimic a real browser
 HEADERS = {
-	"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120 Safari/537.36",
+	"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.224 Safari/537.36",
+	"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
 	"Accept-Language": "en-US,en;q=0.9",
-	"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+	"Accept-Encoding": "gzip, deflate",
 	"Connection": "keep-alive",
+	"Upgrade-Insecure-Requests": "1",
+	"Referer": "https://www.google.com/",
 }
 
 class Fetcher:
